f41462ea21e882966f61fe8f086415eb4de6603c
Johannes <jkanefendt@gmail.com>
2026-03-02 21:02:04 +0100

    Intermediate

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+#include <Fonts/FreeSans9pt7b.h>
+#include <Fonts/FreeSans24pt7b.h>
+#include <Fonts/FreeSans18pt7b.h>
 #include <RunningMedian.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
 #include "tahoma22pt7b.h"
 #include "tahoma24pt7b.h"
@@ -14,8 +20,17 @@
 #include "Rcwl9620DistanceSensor.h"
 
 Rcwl9620DistanceSensor sensor(Serial1);
-RunningMedian average(127);
+RunningMedian average(511);
 Adafruit_SH1107 display = Adafruit_SH1107(128, 128, &Wire, -1, 1000000, 100000);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 600000);
+
+unsigned int wifiLastConnectionAttempt;
+
+unsigned int diameter = 1500;
+unsigned int highMark = 400;
+unsigned int lowMark = 2500;
 
 void setup()
 {
@@ -32,11 +47,25 @@ void setup()
   display.begin(0x3c, false);
   display.setRotation(3);
   display.clearDisplay();
-  display.setFont(&Tahoma18pt7b);
   display.setTextColor(SH110X_WHITE);
 
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(8, 40);
+  display.print("Fuellstand:");
+
   average.clear();
+
+  WiFi.begin("Bluesheep", "6aKH5wJx&3");
+  WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  wifiLastConnectionAttempt = millis();
+
+  timeClient.begin();
+
+  Serial.println("Startgin...");
+
 }
+
+unsigned int timeLastUpdated = 0;
 
 void loop()
 {
@@ -48,11 +77,22 @@ void loop()
     {
       average.add(distance / 10000.0);
 
-      char tmp[32];
-      snprintf(tmp, 16, "%.1f%%", average.getAverage());
+      int distanceMm = average.getAverage() * 10;
 
-      display.clearDisplay();
-      display.setCursor(0, 32);
+      float ratio = (lowMark - distanceMm) / (float)(lowMark - highMark);
+
+      char tmp[32];
+      snprintf(tmp, 16, "%.1f %%", ratio * 100);
+
+      display.fillRect(0, 41, 128, 80, SH110X_BLACK);
+      display.setFont(&FreeSans18pt7b);
+ display.setCursor(4, 72);     
+      display.print(tmp);
+
+      float volumeLiters = PI * pow(diameter * 0.5, 2) * (lowMark - distanceMm) / 1000000;
+
+      snprintf(tmp, 16, "%d", (int) volumeLiters);
+      display.setCursor(4, 105);     
       display.print(tmp);
       display.display();
     }
@@ -61,5 +101,44 @@ void loop()
   {
     sensor.startMeasure();
   }
+
+  wl_status_t wifiStatus = WiFi.status();
+  if (wifiStatus != WL_CONNECTED && millis() - wifiLastConnectionAttempt > 10000) 
+  {
+      Serial.printf("Reconnect %d\n", wifiStatus);
+      WiFi.reconnect();
+      wifiLastConnectionAttempt = millis();
+  }
+
+  if (timeLastUpdated == 0 || millis() - timeLastUpdated > 10000)
+  {
+    if (wifiStatus == WL_CONNECTED)
+    {
+      timeClient.update();
+    }
+ 
+    display.fillRect(0, 0, 128, 17, SH110X_BLACK);
+    display.setFont(&FreeSans9pt7b);
+    display.setCursor(8, 16);
+
+    if (timeClient.isTimeSet())
+    {
+      time_t now = timeClient.getEpochTime();
+      struct tm* timeinfo = localtime(&now);
+      char buf[19];
+      strftime (buf, sizeof(buf), "%d.%m. %R", timeinfo);
+      Serial.println(buf);
+      display.print(buf);
+    }
+    else
+    {
+      display.print("Kein Wlan");
+    }
+
+    display.display();
+
+    timeLastUpdated = millis();
+  }
+
   delay(4);
 }
